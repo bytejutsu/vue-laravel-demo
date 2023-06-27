6c92d132f70a6e7f5b4cdbdf498f9868e0f1cb75
dhia nabli <dhianabli@outlook.com>
2023-06-27 22:13:05 +0200

    used the inertia Link component inside the Home vue Component to add the SPA functionality

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Home</h1>
+    <nav>
+        <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/users">Users</Link></li>
+            <li><Link href="/settings">Settings</Link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: "Home.vue",
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,7 +32,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // });
 
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp, Head, Link } from '@inertiajs/vue3'
 
 createInertiaApp({
     resolve: name => {
@@ -42,6 +42,8 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .component('Head', Head)
+            .component('Link', Link)
             .mount(el)
     },
 })
