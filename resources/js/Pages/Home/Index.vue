1b1963bfd9c42c723d140c4c066bb4e8409a2f96
dhia nabli <dhianabli@outlook.com>
2023-07-02 04:50:26 +0200

    moved Pinia example to the Pinia section

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,15 +1,8 @@
 <template>
-    <todo-app />
+    <h1 class="text-3xl font-bold text-center m-4 p-2">Home Playground</h1>
 </template>
 
 <script setup>
-
-import TodoApp from "./TodoApp.vue";
-import {defineOptions} from "vue";
-
-defineOptions({
-    components: {TodoApp}
-})
 
 </script>
 
--- resources/js/Pages/Pinia/Index.vue
+++ resources/js/Pages/Pinia/Index.vue
@@ -1,8 +1,16 @@
 <template>
-    <h1>Pinia</h1>
+    <h1 class="text-3xl font-bold text-center m-4 p-2">Pinia Playground</h1>
+    <todo-app />
 </template>
 
 <script setup>
+
+import TodoApp from "./TodoApp.vue";
+import {defineOptions} from "vue";
+
+defineOptions({
+    components: {TodoApp}
+})
 
 </script>
 
