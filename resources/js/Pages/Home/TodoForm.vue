97311047546a3be8d79534834c45118d45f04e9f
dhia nabli <dhianabli@outlook.com>
2023-07-02 04:43:47 +0200

    used inertiajs form in TodoForm

--- resources/js/Pages/Home/TodoForm.vue
+++ resources/js/Pages/Home/TodoForm.vue
@@ -1,26 +1,31 @@
 <template>
-    <form @submit.prevent="addItemAndClear(todo)">
+    <form @submit.prevent="addItemAndClear(form.todo)">
         <div class="flex justify-between">
-            <input v-model="todo" type="text" class="w-full m-4 p-2 outline outline-2 outline-blue-400 focus:outline-blue-500"/>
+            <input v-model="form.todo" type="text" class="w-full m-4 p-2 outline outline-2 outline-blue-400 focus:outline-blue-500"/>
             <button type="submit" class="m-4 p-2 bg-blue-500 text-white font-bold">Add</button>
         </div>
     </form>
 </template>
 
 <script setup>
-import { ref } from "vue";
+
 import {useTodosStore} from "../../Stores/useTodosStore.js";
 
-    const todo = ref("");
-    const todosStore = useTodosStore();
+import { useForm } from '@inertiajs/vue3'
 
-    function addItemAndClear(item) {
-        if (item.length === 0) {
-            return;
-        }
-        todosStore.addTodo(item);
-        todo.value = "";
+const form = useForm({
+    todo: "",
+})
+
+const todosStore = useTodosStore();
+
+function addItemAndClear(item) {
+    if (item.length === 0) {
+        return;
     }
+    todosStore.addTodo(item);
+    form.todo = "";
+}
 
 </script>
 
